d7742e99f48a45e410ac5ba6a43ebda30f8f5b75
samaayman97 <sama712153@gmail.com>
2023-07-14 19:24:45 -0700

    a

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,19 +2,24 @@
 
 /**
  * main - main block
- * Description:Use `putchar` function to print the alphabet in lowercase.
+ * Description:Use Print the alphabet in lowercase and uppercase on same line
+ * using putchar only thrice, followed by new line
  * Return:0
  */
 int main(void)
 {
-	char alpha = 'a';
-
-	while (alpha <= 'z')
-	{
-		putchar(alpha);
-		alpha++;
-	}
-	putchar('\n');
-
-	return (0);
+char alpha = 'a';
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+}
+alpha = 'A';
+while (alpha <= 'Z')
+{
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+return (0);
 }
